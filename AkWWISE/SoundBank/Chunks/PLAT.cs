336f609df423b500e8a8c836986908a75c92e792
Reherc <amaury.vanoorenberghe@yahoo.com>
2022-08-21 02:25:42 +0200

    Added PLAT (Platform) chunk parsing

--- AkWWISE/SoundBank/Chunks/PLAT.cs
+++ AkWWISE/SoundBank/Chunks/PLAT.cs
@@ -1,18 +1,38 @@
-﻿using AkWWISE.IO.Interfaces;
+﻿using System;
+using AkWWISE.IO.Interfaces;
 using AkWWISE.SoundBank.Model;
 
 namespace AkWWISE.SoundBank.Chunks
 {
 	public class PLAT : DataChunk
 	{
+		#region Initialization
+
 		public PLAT(SoundBank soundBank) : base(soundBank)
 		{
 		}
 
 		public override ChunkType ChunkType => ChunkType.PLAT;
+		#endregion
+
+		#region Data
+		public string AkCustomPlatformName { get; private set; }
+		#endregion
 
 		public override void Visit(IReader reader)
 		{
+			base.Visit(reader);
+
+			if (AkVersion <= 136)
+			{
+				AkCustomPlatformName = reader.ReadS32STR();
+			}
+			else
+			{
+				AkCustomPlatformName = reader.ReadSTZ();
+			}
+
+			Console.WriteLine($"[PLAT] Platform {AkCustomPlatformName}");
 		}
 	}
 }
